51bb7e1af2b46b150019042d15e3d1270c06c794
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2026-07-13 12:58:02 -0300

    first-commit: init

---
+++ src/test/java/br/com/moviecatalog/movie_catalog_api/MovieCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moviecatalog.movie_catalog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
